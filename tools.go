fa100b7ff126f64572dc3d635f8918c7cc3cc4ee
Caleb Doxsey <cdoxsey@pomerium.com>
2025-02-10 13:07:14 -0700

    remove go-related code (#72)

--- tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-// +build tools
-
-package pomerium_console
-
-import (
-	_ "github.com/golang/protobuf/protoc-gen-go"                // for generating protobuf
-	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc" // for generating docs
-)
